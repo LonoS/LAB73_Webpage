7ba4bcb8dab45edbaacab8d3ee0962727f9f2b9f
LonoS <lonosgames@gmail.com>
2018-04-21 18:28:39 +0200

    Added some file

---
+++ JS_Files/Navigation.js
@@ -0,0 +1,33 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+/* Open the sidenav */
+function openNav(x) {
+    document.getElementById("mySidenav").style.width = "100%";
+
+}
+
+/* Close/hide the sidenav */
+function closeNav(x) {
+    document.getElementById("mySidenav").style.width = "0";
+
+}
+
+var isOpen = false;
+function doNavAction(x) {
+    if(isOpen == true){
+        isOpen = false;
+        closeNav(x);
+        x.classList.remove("change");
+
+
+    }else{
+        isOpen = true;
+        openNav(x);
+        x.classList.toggle("change");
+
+    }
+
+
+}
+
